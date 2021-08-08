b6a64cf2ef1e0988c0eeb8b342a4415eb4832ab3
mridulhossain <mridulfeni94@gmail.com>
2021-08-09 01:18:18 +0600

    Given a non-empty array of integers nums, every element appears twice except for one. Find that single one.

---
+++ ProblemSolving/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ProblemSolving
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            int[] nums = { 4, 1, 2, 1, 2 };
+            Console.WriteLine(SingleNumber(nums));
+ 
+        }
+
+
+        public static int SingleNumber(int[] nums)
+        {
+            Array.Sort(nums);
+            int num = 0;
+
+           
+                for (int i = 0; i < nums.Length; i++)
+                {
+                    num ^= nums[i];
+                }
+          
+
+
+            return num;
+        }
+    }
+
+}
+
